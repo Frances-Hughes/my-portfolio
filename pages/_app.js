3b74dbab6b0597259a112ffee090e235736e6807
Frances Hughes <franceslhughes@hotmail.com>
2024-01-17 23:30:23 +1300

    Started Repo - Navbar and little extras created, started on Home Page
    Started Repo - Navbar and little extras created, started on Home Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+
+import PageLayout from "../components/PageLayout";
+import Transition from "../components/Transition";
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <PageLayout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </PageLayout>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import Image from "next/image";
+
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+import { fadeIn } from "framer-motion";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/80 h-full">
+      <div className="w-full h-full bg-gradient-to-r from-primary/15 via-black/35 to-black/20">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/*title*/}
+          <h1 className="h1">
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            nostrum perspiciatis corporis. Pariatur reiciendis minima facere
+            deserunt? Corrupti, omnis numquam, praesentium possimus modi, amet
+            facilis culpa dignissimos quo ex officia!
+          </p>
+          <div className="flex justify-center xl:hidden">
+            <ProjectsBtn />
+          </div>
+        </div>
+        <div>image</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
