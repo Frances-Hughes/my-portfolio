1243b3dfe9242e92e9a2844c3bbfd6932e78963b
Frances Hughes <franceslhughes@hotmail.com>
2024-01-19 15:58:36 +1300

    Mobile view project button, Header and Social Media
    Fixed mobile view button, did some work on Header including social media.

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,42 @@
+import Link from "next/link";
+
+import {
+  RiLinkedinBoxLine,
+  RiGithubFill,
+  RiInstagramLine,
+  RiFacebookLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-xl">
+      <Link
+        href={"https://www.linkedin.com/in/frances-l-hughes/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinBoxLine />
+      </Link>
+      <Link
+        href={"https://github.com/Frances-Hughes"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubFill />
+      </Link>
+
+      <Link
+        href={"https://www.facebook.com/ViveLaFrances/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiFacebookLine />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/francesroundtheworld/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
